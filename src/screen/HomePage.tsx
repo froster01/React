78d1645247e2712c4622a6aab36e6922e6237969
froster01 <syafiqafro@gmail.com>
2024-07-16 01:09:14 +0800

    Settle tutorial with figma

--- src/screen/HomePage.tsx
+++ src/screen/HomePage.tsx
@@ -1,28 +1,216 @@
-import {View, Text, StyleSheet, Button} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import * as React from "react";
+import { StyleSheet, View, Image, Text } from "react-native";
+import { FontSize, Color, FontFamily, Border, Padding } from "../styles/GlobalStyles";
 
-// Please change this parent function and filename to ScreenOne. If you prefer HomePage, please change it inside your stack navigator file as well
-const HomePage = () => {
-  const navigation: any = useNavigation();
+const Homepage = () => {
   return (
-    <View style={styles.container}>
-      <Button title="Click me" onPress={() => navigation.navigate('AboutMe')} />
-      <Text style={styles.text}>HomePage</Text>
+    <View style={styles.iphone1415ProMax1Parent}>
+      <View style={styles.iphone1415ProMax1}>
+        <View
+          style={[styles.iphone1415ProMax1Child, styles.iphone1415Position]}
+        />
+        <Image
+          style={styles.dAvatars10}
+          resizeMode="cover"
+          source={require("../assets/3d-avatars--10.png")}
+        />
+        <Text style={[styles.thisIsMy, styles.thisIsMyTypo]}>
+          This is my Homepage designed using Figma
+        </Text>
+        <View
+          style={[styles.iphone1415ProMax1Item, styles.iphone1415Position]}
+        />
+        <Text style={[styles.wantToLearn, styles.labelTextTypo]}>
+          want to learn more about me? Press the button below !
+        </Text>
+        <Text style={[styles.testingUsingFigma, styles.labelTextTypo]}>
+          Testing using Figma zzzz
+        </Text>
+      </View>
+      <View style={styles.topAppBar}>
+        <View style={[styles.leadingIcon, styles.stateIconFlexBox]}>
+          <View style={[styles.container, styles.buttonFlexBox]}>
+            <View style={[styles.stateLayer, styles.stateIconFlexBox]}>
+              <Image
+                style={styles.icon}
+                resizeMode="cover"
+                source={require("../assets/icon.png")}
+              />
+            </View>
+          </View>
+        </View>
+        <Text style={[styles.headline, styles.thisIsMyTypo]}>Homepage</Text>
+        <View style={[styles.trailingIcon, styles.stateIconFlexBox]}>
+          <View style={[styles.container, styles.buttonFlexBox]}>
+            <View style={[styles.stateLayer, styles.stateIconFlexBox]}>
+              <Image
+                style={styles.icon}
+                resizeMode="cover"
+                source={require("../assets/icon.png")}
+              />
+            </View>
+          </View>
+        </View>
+      </View>
+      <View style={[styles.button, styles.buttonFlexBox]}>
+        <View style={[styles.stateLayer2, styles.stateIconFlexBox]}>
+          <Text style={styles.labelTextTypo}>About Me</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  iphone1415Position: {
+    width: 430,
+    left: 0,
+    position: "absolute",
+  },
+  thisIsMyTypo: {
+    textAlign: "center",
+    lineHeight: 38,
+    fontSize: FontSize.size_13xl,
+  },
+  labelTextTypo: {
+    color: Color.schemesOnPrimary,
+    fontFamily: FontFamily.m3HeadlineMedium,
+    lineHeight: 36,
+    fontSize: FontSize.m3HeadlineMedium_size,
+    textAlign: "center",
+  },
+  stateIconFlexBox: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonFlexBox: {
+    borderRadius: Border.br_81xl,
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+  },
+  iphone1415ProMax1Child: {
+    backgroundColor: "#d9d9d9",
+    top: 0,
+    left: 0,
+    height: 932,
+  },
+  dAvatars10: {
+    top: 127,
+    left: 155,
+    width: 120,
+    height: 120,
+    position: "absolute",
+  },
+  thisIsMy: {
+    top: 277,
+    left: 18,
+    fontWeight: "600",
+    fontFamily: FontFamily.robotoBold,
+    color: "#000",
+    width: 394,
+    height: 93,
+    position: "absolute",
+  },
+  iphone1415ProMax1Item: {
+    top: 423,
+    borderTopLeftRadius: Border.br_13xl,
+    borderTopRightRadius: Border.br_13xl,
+    backgroundColor: "#7491a0",
+    height: 509,
+  },
+  wantToLearn: {
+    top: 473,
+    left: 29,
+    position: "absolute",
+  },
+  testingUsingFigma: {
+    top: 581,
+    left: 58,
+    position: "absolute",
+  },
+  iphone1415ProMax1: {
+    backgroundColor: Color.schemesOnPrimary,
+    overflow: "hidden",
+    width: 430,
+    left: 0,
+    top: 0,
+    position: "absolute",
+    height: 932,
+  },
+  icon: {
+    width: 24,
+    height: 24,
+  },
+  stateLayer: {
+    padding: Padding.p_5xs,
+    flexDirection: "row",
+  },
   container: {
+    flexDirection: "row",
+  },
+  leadingIcon: {
+    height: 48,
+    width: 48,
+    justifyContent: "center",
+  },
+  headline: {
+    fontFamily: FontFamily.interRegular,
+    color: Color.schemesOnSurface,
+    textShadowColor: "rgba(0, 0, 0, 0.25)",
+    textShadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    textShadowRadius: 4,
+    marginLeft: 6,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-  text: {
-    fontSize: 30,
-    color: 'black',
+  trailingIcon: {
+    marginLeft: 6,
+    height: 48,
+    width: 48,
+    justifyContent: "center",
   },
-  Button: {},
+  topAppBar: {
+    shadowColor: "rgba(0, 0, 0, 0.25)",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowRadius: 4,
+    elevation: 4,
+    shadowOpacity: 1,
+    backgroundColor: Color.schemesSurface,
+    paddingHorizontal: Padding.p_9xs,
+    paddingVertical: Padding.p_5xs,
+    alignItems: "center",
+    flexDirection: "row",
+    width: 430,
+    left: 0,
+    top: 0,
+    position: "absolute",
+  },
+  stateLayer2: {
+    alignSelf: "stretch",
+    paddingHorizontal: Padding.p_5xl,
+    paddingVertical: Padding.p_3xs,
+    flexDirection: "row",
+    flex: 1,
+  },
+  button: {
+    top: 666,
+    left: 49,
+    backgroundColor: Color.schemesPrimary,
+    width: 332,
+    height: 64,
+    position: "absolute",
+  },
+  iphone1415ProMax1Parent: {
+    width: "100%",
+    height: 932,
+    flex: 1,
+  },
 });
 
-export default HomePage;
+export default Homepage;
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,37 @@
+/* fonts */
+    export const FontFamily = {
+    m3HeadlineMedium: "Roboto-Regular",
+    interRegular: "Inter-Regular",
+    robotoBold: "Roboto-Bold",
+    m3LabelLarge: "Roboto-Medium",
+    };
+
+    /* font sizes */
+    
+    export const FontSize = {
+    m3HeadlineMedium_size: 28,
+    size_13xl: 32,
+    m3TitleLarge_size: 22,
+    m3LabelLarge_size: 14,
+    };
+  /* Colors */
+    
+    export const Color = {
+    schemesPrimary: "#65558f",
+    schemesOnPrimary: "#fff",
+    schemesSurface: "#fef7ff",
+    schemesOnSurface: "#1d1b20",
+    };
+
+    /* Paddings */
+    export const Padding = {
+    p_5xl: 24,
+    p_3xs: 10,
+    p_9xs: 4,
+    p_5xs: 8,
+    };
+  /* border radiuses */
+    export const Border = {
+    br_81xl: 100,
+    br_13xl: 32,
+    };
